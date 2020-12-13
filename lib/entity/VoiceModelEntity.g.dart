6f26c755d3e38bede8e761bc254e79381411a206
leetomlee123 <leetomlee123@gmail.com>
2020-12-13 21:22:39 +0800

    添加听书

---
+++ lib/entity/VoiceModelEntity.g.dart
@@ -0,0 +1,19 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'VoiceModelEntity.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+VoiceModelEntity _$VoiceModelEntityFromJson(Map<String, dynamic> json) {
+  return VoiceModelEntity(
+      json['cover'] as String, json['link'] as String, json['idx'] as int);
+}
+
+Map<String, dynamic> _$VoiceModelEntityToJson(VoiceModelEntity instance) =>
+    <String, dynamic>{
+      'cover': instance.cover,
+      'link': instance.link,
+      'idx': instance.idx
+    };
