79d2016e517b5fde7977498ef7395899f0f0f2eb
leetomlee123 <leetomlee123@gmail.com>
2026-07-19 12:34:13 +0800

    fix: use PrefsKeys for auth header value

--- lib/common/auth_interceptor.dart
+++ lib/common/auth_interceptor.dart
@@ -6,7 +6,7 @@ class AuthInterceptor extends Interceptor {
   @override
   void onRequest(RequestOptions options, RequestInterceptorHandler handler) {
     if (SpUtil.haveKey(PrefsKeys.auth)) {
-      options.headers.addAll(({"auth": SpUtil.getString("auth")}));
+      options.headers.addAll(({"auth": SpUtil.getString(PrefsKeys.auth)}));
     }
     options.headers.addAll(({"user-agent":"Mozilla/5.0 (Linux; Android 11; KB2000) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4515.131 Mobile Safari/537.36"}));
     return handler.next(options);
