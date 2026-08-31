19f98d1781fd414056fa608e9f7653aa904fcafc
leetomlee123 <leetomlee123@gmail.com>
2026-08-31 11:32:34 +0000

    build(android): enable core library desugaring for ota_update compatibility

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -23,6 +23,7 @@ android {
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
+        isCoreLibraryDesugaringEnabled = true
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
     }
@@ -80,4 +81,8 @@ kotlin {
 
 flutter {
     source = "../.."
+}
+
+dependencies {
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.1.4")
 }
